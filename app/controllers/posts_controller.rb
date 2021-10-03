ee07278d34cb4043810b62d46dff2da13500bc24
anniezh12 <aniqaelahi@yahoo.com>
2021-10-03 00:22:48 -0700

    first commit

---
+++ app/controllers/posts_controller.rb
@@ -0,0 +1,30 @@
+class PostsController < ApplicationController
+    def index
+        @posts = Post.all
+    end
+    def new
+        redirect_to @posts
+    end
+
+    def show
+        @post = Post.find(params[:id])
+    end
+
+    def create
+        #following statement will show params submitted by the form in posts/new.html.erb
+        #render plain: params[:post].inspect
+        @post = Post.new(post_params)
+        
+        if @post.save
+          redirect_to @post
+        else
+            render 'new'
+        end 
+
+    end   
+    
+    private def post_params
+        params.require(:post).permit(:title, :body)
+    end
+    
+end
